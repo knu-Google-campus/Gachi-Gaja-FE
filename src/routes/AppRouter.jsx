ccbb532934aa0efca3f898c03de435510cf8c5fc
siwoo <pty0714@naver.com>
2025-09-24 00:38:41 +0900

    Add Login Page Route

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Landingpage from "../pages/LandingPage";
+import Loginpage from "../pages/LoginPage";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landingpage />} />
+        <Route path="/login" element={<Loginpage />} />
       </Routes>
     </BrowserRouter>
   );

762392805ba65be415c63d233c36086a58b4b5fa
siwoo <pty0714@naver.com>
2025-09-24 00:38:41 +0900

    Add Login Page Route

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,29 @@
+"use client"
+import styled from "styled-components"
+import Button from "../components/common/Button"
+import Header from "../components/common/Header"
+import Footer from "../components/common/Footer"
+import Inner from "../components/layout/Inner"
+
+const PageContainer = styled.div`
+  min-height: 100vh;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+`
+
+const LoginSection = styled.div`
+
+`
+
+const LoginTitle = styled.div`
+
+`
+
+export default function Login() {
+    return (
+        <PageContainer>
+            
+        </PageContainer>
+    );
+}

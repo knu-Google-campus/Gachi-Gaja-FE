a4d69aa0952c1dbe25f3ed885a5412d45370f191
faddishcorn <juni129461@gmail.com>
2025-09-25 15:29:27 +0900

    chore: route setting

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Landingpage from "../pages/LandingPage";
 import Loginpage from "../pages/LoginPage";
+import MainLayout from "../components/layout/MainLayout";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,10 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Landingpage />} />
         <Route path="/login" element={<Loginpage />} />
+
+        <Route element={<MainLayout />}>
+          {/* 메인 레이아웃 사용할 페이지들 여기에 추가 */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );

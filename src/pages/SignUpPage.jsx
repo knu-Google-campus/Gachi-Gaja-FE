3cd3bba93eaa87c1fafae1f175a53311af32abbe
siwoo <pty0714@naver.com>
2025-10-03 01:16:41 +0900

    Sign-Up Page Base

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,7 @@
+"use client"
+
+import styled from "styled-components"
+import Button from "../components/common/Button"
+import Header from "../components/common/Header"
+import Footer from "../components/common/Footer"
+import { useState } from "react"

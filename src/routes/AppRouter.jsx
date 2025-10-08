a5cb0ca23ac92f2902f3368347c63877c44b1fea
faddishcorn <juni129461@gmail.com>
2025-10-08 18:04:10 +0900

    chore: 라우팅 설정

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,10 @@
-import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Landingpage from "../pages/LandingPage";
 import Loginpage from "../pages/LoginPage";
 import MainLayout from "../components/layout/MainLayout";
+import TravelRoomsPage from "../pages/TravelRoomsPage";
+import CreateTripPage from "../pages/CreateTripPage";
+import TripRoomPage from "../pages/TripRoomPage";
 
 const AppRouter = () => {
   return (
@@ -13,7 +15,9 @@ const AppRouter = () => {
 
         <Route element={<MainLayout />}>
           {/* 메인 레이아웃 사용할 페이지들 여기에 추가 */}
-          
+          <Route path="/travel-rooms" element={<TravelRoomsPage />} />
+          <Route path="/create-trip" element={<CreateTripPage />} />
+          <Route path="/trip-room/:id" element={<TripRoomPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
